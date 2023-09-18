891fd69fa83b2271875ce08bc7015080fc14266b
profit0124 <profit0124@gmail.com>
2023-09-18 20:23:03 +0900

    Initial Commit

---
+++ ShutterButtonAnimation/ShutterButtonAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShutterButtonAnimationApp.swift
+//  ShutterButtonAnimation
+//
+//  Created by Sooik Kim on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct ShutterButtonAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
